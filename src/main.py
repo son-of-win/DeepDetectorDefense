1bc755c6f570cb3988efce8d5efa0c281dd965ac
son-of-win <hackeranonymous1310@gmail.com>
2022-03-10 00:05:42 +0700

    test mnist

--- src/main.py
+++ src/main.py
@@ -3,47 +3,31 @@ from keras.models import load_model
 from classifier import AlexnetClassifier
 from utils import *
 from detector import *
-
+from test_accuracy import test_mnist_data, test_mnist_only_scalar
 
 # prepare data
+
+# origin data
 (x_train, y_train), (x_val, y_val), (x_test, y_test) = data_preprocessing_mnist(0.2)
-adv = np.load('../data/fgsm_mnist_alexnet_origin=9_target=7_weight=0.35.npy')
+
+# adv data
+# adv1 = np.load('../data/fgsm_mnist_alexnet_origin=9_target=7_weight=0.35.npy')
+# adv2 = np.load('../data/fgsm_mnist_alexnet_origin=9_target=7_weight=0.6.npy')
+# adv3 = np.load('../data/fgsm_mnist_alexnet_origin=9_target=7_weight=0.122.npy')
+# adv = np.load('../data/cw_l2.npy')
+# adv = np.concatenate((adv1, adv2, adv3))
+adv = np.load('../data/lbfgs_origin=7_target=9_weight=0.43.npy')
 adv_label = np.full(len(adv), 9)
+
+x_test = x_test[0:len(adv)]
+y_test = y_test[0:len(adv)]
 train_data, train_label, is_adv = shuffle_data(x_test, y_test, adv, adv_label)
 
 Classifier = AlexnetClassifier()
 Classifier.load("lenet.h5")
 detectorFilter = DefenderModel(28, 1)
 print(Classifier.evaluate(x_test, y_test))
-TN = 0
-TP = 0
-FN = 0
-FP = 0
-for i in range(len(train_data)):
-    # current_label = int(np.argmax(train_label[i]))
-    current_label_no_filter = int(np.argmax(Classifier.classifier.predict(train_data[i].reshape((1, 28, 28, 1)))))
-    temp = np.reshape(train_data[i], (detectorFilter.image_channel, detectorFilter.image_size, detectorFilter.image_size))
-    imageEntropy = detectorFilter.caculateEntropy(temp)
-    if imageEntropy < 4:
-        current_image_res = detectorFilter.scalarQuantization(temp, 128)
-    elif imageEntropy < 5:
-        current_image_res = detectorFilter.scalarQuantization(temp, 64)
-    else:
-        current_image_ASQ = detectorFilter.scalarQuantization(temp, 43)
-        current_image_crossMean = detectorFilter.crossAndDiamondMeanFilter(current_image_ASQ, 3, 25, 5)
-        current_image_res = detectorFilter.chooseCloserFilter(imageEntropy, current_image_ASQ, current_image_crossMean)
-    current_x_res = np.reshape(current_image_res, (1, 28, 28, 1))
-    current_x_label_predict = np.argmax(Classifier.classifier.predict(current_x_res))
 
-    if current_label_no_filter != current_x_label_predict:
-        if is_adv[i]:
-            TP += 1
-        else:
-            FP += 1
-    else:
-        if is_adv[i]:
-            FN += 1
-        else:
-            TN += 1
-print("num origin: %d \nnum adv: %d" %(len(x_test), len(adv)))
-print("TP: %d, FP: %d, FN: %d, TN: %d" % (TP, FP, FN, TN))
+print("num origin: %d \nnum adv: %d" % (len(x_test), len(adv)))
+# test_mnist_data(train_data, train_label, is_adv, Classifier, detectorFilter)
+test_mnist_only_scalar(train_data, train_label, is_adv, Classifier, detectorFilter)
---
+++ src/test_accuracy.py
@@ -0,0 +1,79 @@
+from utils import *
+import time
+
+
+def test_mnist_data(train_data, train_label, is_adv, Classifier, detectorFilter):
+    TN = 0
+    TP = 0
+    FN = 0
+    FP = 0
+    startTime = time.time()
+    for i in range(len(train_data)):
+        # current_label = int(np.argmax(train_label[i]))
+        current_label_no_filter = int(np.argmax(Classifier.classifier.predict(train_data[i].reshape((1, 28, 28, 1)))))
+        temp = np.reshape(train_data[i],
+                          (detectorFilter.image_channel, detectorFilter.image_size, detectorFilter.image_size))
+        imageEntropy = detectorFilter.caculateEntropy(temp)
+        if imageEntropy < 4:
+            current_image_res = detectorFilter.scalarQuantization(temp, 2)
+        elif imageEntropy < 5:
+            current_image_res = detectorFilter.scalarQuantization(temp, 4)
+        else:
+            current_image_ASQ = detectorFilter.scalarQuantization(temp, 6)
+            current_image_crossMean = detectorFilter.crossAndDiamondMeanFilter(current_image_ASQ, 7, 'cross')
+            current_image_res = detectorFilter.chooseCloserFilter(imageEntropy, current_image_ASQ,
+                                                                  current_image_crossMean)
+        current_x_res = np.reshape(current_image_res, (1, 28, 28, 1))
+        current_x_label_predict = np.argmax(Classifier.classifier.predict(current_x_res))
+
+        if current_label_no_filter != current_x_label_predict:
+            if is_adv[i]:
+                TP += 1
+            else:
+                FP += 1
+        else:
+            if is_adv[i]:
+                FN += 1
+            else:
+                TN += 1
+    # print("num origin: %d \nnum adv: %d" %(len(x_test), len(adv)))
+    endTime = time.time()
+    # recall = TP / (TP + FN)
+    # precision = TP / (TP + FP)
+    print("time run: %.4f" % (endTime - startTime))
+    # print("Recall: %.4f\nPrecision: %.4f"%(recall, precision))
+    print("TP: %d, FP: %d, FN: %d, TN: %d \n" % (TP, FP, FN, TN))
+
+def test_mnist_only_scalar(train_data, train_label, is_adv, Classifier, detectorFilter):
+    TN = 0
+    TP = 0
+    FN = 0
+    FP = 0
+    startTime = time.time()
+    for i in range(len(train_data)):
+        current_label_no_filter = int(np.argmax(Classifier.classifier.predict(train_data[i].reshape((1, 28, 28, 1)))))
+        temp = np.reshape(train_data[i],
+                          (detectorFilter.image_channel, detectorFilter.image_size, detectorFilter.image_size))
+        imageEntropy = detectorFilter.caculateEntropy(temp)
+        if imageEntropy < 4:
+            current_image_res = detectorFilter.scalarQuantization(temp, 2)
+        elif imageEntropy < 5:
+            current_image_res = detectorFilter.scalarQuantization(temp, 4)
+        else:
+            current_image_res = detectorFilter.scalarQuantization(temp, 6)
+        current_x_res = np.reshape(current_image_res, (1, 28, 28, 1))
+        current_x_label_predict = np.argmax(Classifier.classifier.predict(current_x_res))
+        if current_label_no_filter != current_x_label_predict:
+            if is_adv[i]:
+                TP += 1
+            else:
+                FP += 1
+        else:
+            if is_adv[i]:
+                FN += 1
+            else:
+                TN += 1
+    endTime = time.time()
+    print("time run: %.4f" % (endTime - startTime))
+    # print("Recall: %.4f\nPrecision: %.4f"%(recall, precision))
+    print("TP: %d, FP: %d, FN: %d, TN: %d \n" % (TP, FP, FN, TN))
