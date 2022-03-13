23171864eb03223d01f3faf587d2740ba9900d86
son-of-win <hackeranonymous1310@gmail.com>
2022-03-13 11:19:37 +0700

    update requirement, config

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DeepDetectorDefense
-https://drive.google.com/drive/folders/1fXtNvX0hR0fc3C69aauA5orCjPeO9QQh?usp=sharing
+
+# Running tool
+```shell
+$ git clone https://github.com/son-of-win/DeepDetectorDefense.git
+$ cd DeepDetectorDefense
+$ virtualenv -p python3 .venv
+$ .venv\Scripts\activate (Windows)
+  source .venv\bin\activate (Linux)
+$ pip3 install -r requirements.txt
+$ cd src
+$ python main.py
+```
+
+[Link adv](https://drive.google.com/drive/folders/1fXtNvX0hR0fc3C69aauA5orCjPeO9QQh?usp=sharing)
+
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os.path
+
 import numpy as np
 from keras.models import load_model
 from classifier import AlexnetClassifier
@@ -5,11 +7,14 @@ from utils import *
 from detector import *
 from test_accuracy import *
 from tuning_hyperparam import *
+from configparser import ConfigParser
 
+config = ConfigParser()
+config.read('../config.ini')
 # prepare data
 
 # origin data
-(x_train, y_train), (x_val, y_val), (x_test, y_test) = data_preprocessing_cifar10(0.2)
+(x_train, y_train), (x_val, y_val), (x_test, y_test) = data_preprocessing_cifar10(float(config.get('DATA','validationRatio')))
 
 # adv data
 # mnist
@@ -19,22 +24,22 @@ from tuning_hyperparam import *
 # adv4 = np.load('../data/cw_l2.npy')
 # adv = np.concatenate((adv1, adv2, adv3))
 # cifar
-adv1 = np.load('../data/cifar10/cifar_fgsm_l2_0_250.npy')
-adv2 = np.load('../data/cifar10/cifar_fgsm_l2_0_500.npy')
+adv1 = np.load(config.get("DATA", "advDataFolderPath") + '/cifar10/cifar_fgsm_l2_0_250.npy')
+adv2 = np.load(config.get("DATA", "advDataFolderPath") + '/cifar10/cifar_fgsm_l2_0_500.npy')
 adv = np.concatenate((adv1, adv2))
-
-adv_label = np.full(len(adv), 9)
-x_train = x_train[0:len(adv)]
-y_train = y_train[0:len(adv)]
-train_data, train_label, is_adv = shuffle_data(x_train, y_train, adv, adv_label)
-
+#
+adv_label = np.full(len(adv), config.getint("DATA", "advDataLabel"))
+x_test = x_test[0:len(adv)]
+y_test = y_test[0:len(adv)]
+train_data, train_label, is_adv = shuffle_data(x_test, y_test, adv, adv_label)
+#
 Classifier = AlexnetClassifier()
-Classifier.load("cifar10_classifier.h5")
-# detectorFilter = DefenderModel(28, 1)
-detectorFilter = DefenderModel(32,3)
+Classifier.load(config.get("CLASSIFIER", 'targetClassifierPath'))
+# # detectorFilter = DefenderModel(28, 1)
+detectorFilter = DefenderModel(32, 3)
 print(Classifier.evaluate(x_test, y_test))
 
-print("num origin: %d \nnum adv: %d" % (len(x_train), len(adv)))
+print("num origin: %d \nnum adv: %d" % (len(x_test), len(adv)))
 test_cifar_data(train_data, train_label, is_adv, Classifier, detectorFilter)
 # test_mnist_only_scalar(train_data, train_label, is_adv, Classifier, detectorFilter)
 # IntervalTuningMnist(train_data, train_label, is_adv, Classifier, detectorFilter)
